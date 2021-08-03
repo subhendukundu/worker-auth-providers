3dab3a3e89c1b81213d293a20edfba6ae35b42bf
subh <subhendukundu14@gmail.com>
2021-08-03 13:27:52 +0530

    fix: add state as options

--- src/providers/google/redirect.js
+++ src/providers/google/redirect.js
@@ -2,16 +2,27 @@ import * as queryString from 'query-string';
 import { ConfigError } from '../../utils/errors';
 
 export default async function redirect({ options }) {
-  const { clientId, redirectUrl } = options;
-  const params = queryString.stringify({
-    client_id: clientId,
-    redirect_uri: redirectUrl,
-    response_type: 'code',
-    scope: 'openid email profile',
-    include_granted_scopes: 'true',
-    state: 'pass-through value',
-  });
+	const {
+		clientId,
+		redirectUrl,
+		scope = 'openid email profile',
+		responseType = 'code',
+		state = 'pass-through value'
+	} = options;
+	if (!clientId) {
+		throw new ConfigError({
+			message: 'No client id passed'
+		});
+	}
+	const params = queryString.stringify({
+		client_id: clientId,
+		redirect_uri: redirectUrl,
+		response_type: responseType,
+		scope,
+		include_granted_scopes: 'true',
+		state
+	});
 
-  const googleLoginUrl = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
-  return googleLoginUrl;
+	const url = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+	return url;
 }
