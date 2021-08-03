8c4d49cbcacb6e4e678a8430f0f9b7fdbf217868
subh <subhendukundu14@gmail.com>
2021-08-04 00:28:39 +0530

    feat: spotify login

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import * as awsSNS from './providers/aws-sns';
 import * as twilio from './providers/twilio';
 import * as facebook from './providers/facebook';
 import * as discord from './providers/discord';
+import * as spotify from './providers/spotify';
 
-export { github, google, awsSNS, twilio, facebook, discord };
+export { github, google, awsSNS, twilio, facebook, discord, spotify };
---
+++ src/providers/spotify/redirect.js
@@ -0,0 +1,27 @@
+import * as queryString from 'query-string';
+import { ConfigError } from '../../utils/errors';
+
+export default async function redirect({ options }) {
+	const {
+		clientId,
+		redirectUrl,
+		scope = 'user-library-read playlist-modify-private',
+		responseType = 'code',
+		showDialog = false
+	} = options;
+	if (!clientId) {
+		throw new ConfigError({
+			message: 'No client id passed'
+		});
+	}
+	const params = queryString.stringify({
+		client_id: clientId,
+		redirect_uri: redirectUrl,
+		response_type: responseType,
+		scope,
+		show_dialog: showDialog
+	});
+
+	const url = `https://accounts.spotify.com/authorize?${params}`;
+	return url;
+}
