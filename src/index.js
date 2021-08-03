30c7bedd5a43919a2d030cdf2488e672845717d0
subh <subhendukundu14@gmail.com>
2021-08-03 20:07:06 +0530

    feat: facebook provider added

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import * as github from './providers/github';
 import * as google from './providers/google';
 import * as awsSNS from './providers/aws-sns';
 import * as twilio from './providers/twilio';
+import * as facebook from './providers/facebook';
 
-export { github, google, awsSNS, twilio };
+export { github, google, awsSNS, twilio, facebook };
