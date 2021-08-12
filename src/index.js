4b3d6526b555eae063e53a2b895dde02dc67476e
subh <subhendukundu14@gmail.com>
2021-08-12 12:07:54 +0530

    feat: email auth

--- src/index.js
+++ src/index.js
@@ -5,5 +5,17 @@ import * as twilio from './providers/twilio';
 import * as facebook from './providers/facebook';
 import * as discord from './providers/discord';
 import * as spotify from './providers/spotify';
+import * as sendgridEmail from './providers/sendgrid-email';
+import * as mailgunEmail from './providers/mailgun-email';
 
-export { github, google, awsSNS, twilio, facebook, discord, spotify };
+export {
+	github,
+	google,
+	awsSNS,
+	twilio,
+	facebook,
+	discord,
+	spotify,
+	sendgridEmail,
+	mailgunEmail
+};
