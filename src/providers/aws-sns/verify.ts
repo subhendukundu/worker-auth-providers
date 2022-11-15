6627589b1a9d40703ca9d418796945299ece6b30
Subhendu Kundu <subhendukundu14@gmail.com>
2022-11-15 18:01:34 +0000

    feat: putting sns back

---
+++ src/providers/aws-sns/verify.ts
@@ -0,0 +1,38 @@
+import jwt from '@tsndr/cloudflare-worker-jwt';
+import { ProviderVerifyOtpError } from '../../utils/errors';
+
+function generateJWT({ secret, phone, claims }) {
+	const customClaims = claims || {
+		id: phone
+	};
+	console.log('[claims, scret]', customClaims, secret);
+	return jwt.sign({ exp: '24h', ...customClaims}, secret, { algorithm: 'HS256' });
+}
+
+export default async function verify({ options }) {
+	const { kvProvider, phone, otp, secret, claims } = options;
+
+	const storedOtp = await kvProvider.get(phone);
+
+	if (!storedOtp || Number(otp) !== Number(storedOtp)) {
+		throw new ProviderVerifyOtpError({
+			message: 'OTP did not match!'
+		});
+	}
+
+	const token = secret ? generateJWT({
+		secret,
+		phone,
+		claims
+	}) : null;
+
+	console.log(token);
+	await kvProvider.delete(phone);
+
+	return token ? {
+		id: phone,
+		token
+	} : {
+		id: phone
+	};
+}
