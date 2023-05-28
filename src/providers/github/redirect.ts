0a0e498b2acadb118b021c11339239a2ecc6966b
Subhendu Kundu <subhendukundu@Subhendus-MacBook-Pro.local>
2023-05-28 10:24:32 +0530

    feat: login to include redirect url

--- src/providers/github/redirect.ts
+++ src/providers/github/redirect.ts
@@ -3,21 +3,39 @@ import { ConfigError } from "../../utils/errors";
 
 type Options = {
   clientId: string;
+  redirectTo?: string;
+  scope?: string[];
+  allowSignup?: boolean;
 };
 
+type Params = {
+  client_id: string;
+  redirect_uri?: string;
+  scope: string;
+  allow_signup?: boolean;
+};
+
+const DEFAULT_SCOPE = ["read:user", "user:email"];
+const DEFAULT_ALLOW_SIGNUP = true;
+
 export default async function redirect({ options }: { options: Options }): Promise<string> {
-  const { clientId } = options;
+  const { clientId, redirectTo, scope = DEFAULT_SCOPE, allowSignup = DEFAULT_ALLOW_SIGNUP } = options;
   if (!clientId) {
     throw new ConfigError({
       message: "No client id passed"
     });
   }
-  const params = queryString.stringify({
+  const params: Params = {
     client_id: clientId,
-    scope: ["read:user", "user:email"].join(" "),
-    allow_signup: true
-  });
+    scope: scope.join(" "),
+    allow_signup: allowSignup,
+  };
 
-  const githubLoginUrl = `https://github.com/login/oauth/authorize?${params}`;
+  if (redirectTo) {
+    params.redirect_uri = redirectTo;
+  }
+
+  const paramString = queryString.stringify(params);
+  const githubLoginUrl = `https://github.com/login/oauth/authorize?${paramString}`;
   return githubLoginUrl;
 }
